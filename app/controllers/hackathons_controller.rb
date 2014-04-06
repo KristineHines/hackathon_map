24971e51a61bb35b1d88597b77a9e87f9f793b1f
Kristine Hines <hines.kristine@gmail.com>
2014-04-05 22:45:21 -0700

    successful call to the hackerleague api, can list hackathons with title, description and image

--- app/controllers/hackathons_controller.rb
+++ app/controllers/hackathons_controller.rb
@@ -1,4 +1,8 @@
 class HackathonsController < ApplicationController
+
   def show
+    path = "http://hackerleague.org/api/v1/hackathons.json"
+    @hackathons = HTTParty.get(path)
+    puts @hackathons
   end
 end
