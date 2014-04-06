e6537bff73ef5b8e7991ad932aecee8a8d73e617
Kristine Hines <hines.kristine@gmail.com>
2014-04-05 21:24:56 -0700

    Initial commit with Rails 4 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackathonMap::Application.config.secret_key_base = '335780b6596dd4efa974b8e8c5860ee8f5b1e66a2a4044a0fe83103fed19482722e641c0f62cf99a7ef550a8fdbc499e1c6683584034e2bd543d66abf30e45fd'
