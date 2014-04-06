f4890ec8381c3377187ac5ddb128881068868cbb
Kristine Hines <hines.kristine@gmail.com>
2014-04-05 21:58:27 -0700

    created hackathon controller, model, and show view. Added the hackathon show view as the root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ HackathonMap::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'hackathons#show'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
