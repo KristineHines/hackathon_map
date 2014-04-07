6a8d7a5f05db69b70491ef61e556733ef338b9e4
Kristine Hines <hines.kristine@gmail.com>
2014-04-06 23:02:26 -0700

    Added bootstrap and some styling.  Listing the hackathons in tiles

--- app/controllers/hackathons_controller.rb
+++ app/controllers/hackathons_controller.rb
@@ -4,21 +4,24 @@ class HackathonsController < ApplicationController
     @hackathons = HTTParty.get(path)
     @hackathons.each do |hackathon|
       if Hackathon.find_by_hackathon_id(hackathon["id"]) == nil
-      Hackathon.create(hackathon_id: hackathon["id"],
-                            address: "#{hackathon["location"]["city"]},
-                                      #{hackathon["state"]}
-                                      #{hackathon["country"]}",
-                                png: hackathon["logo"],
-                               name: hackathon["name"],
-                                url: hackathon["url"])
+        Hackathon.create(hackathon_id: hackathon["id"],
+                              address: "#{hackathon["location"]["city"]},
+                                        #{hackathon["state"]}
+                                        #{hackathon["country"]}",
+                                  png: hackathon["logo"],
+                                 name: hackathon["name"],
+                                  url: hackathon["url"])
       end
     end
+    get_markers
+  end
+
+  def get_markers
     @all_hacks = Hackathon.all
     @hash = Gmaps4rails.build_markers(@all_hacks) do |hackathon, marker|
       marker.infowindow "<h3>#{hackathon.name}</h3><a href=#{hackathon.url}>#{hackathon.url}</a>"
       marker.lat hackathon.latitude
       marker.lng hackathon.longitude
     end
-    puts @hash
   end
 end
